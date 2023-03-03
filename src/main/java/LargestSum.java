a4aa4d826a9d3f145e7e38a9d598bcec13955af5
vtran91 <vtran16@bellsouth.net>
2023-03-03 04:44:47 +0000

    Completed Largest Sum

--- src/main/java/LargestSum.java
+++ src/main/java/LargestSum.java
@@ -1,5 +1,10 @@
 
+import java.util.Collection;
+import java.util.Collections;
 import java.util.List;
+
+import javafx.css.Size;
+import javafx.print.Collation;
 
 public class LargestSum {
     /**
@@ -10,6 +15,7 @@ public class LargestSum {
      * @return the largest possible sum of separate numbers from nums.
      */
     public int bigSum(List<Integer> nums){
-        return 0;
+        Collections.sort(nums);
+        return nums.get(nums.size() - 1) + nums.get(nums.size()-2);
     }
 }
